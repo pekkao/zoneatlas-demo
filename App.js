94b6cecad1c79ca0ed66dcc5e64a3a46c6501e54
pekkao <pekka.ojala@oamk.fi>
2024-03-20 15:09:47 +0200

    Basic version

--- App.js
+++ App.js
@@ -1,13 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const URL = "https://opendata.zoneatlas.com/oulu/objects.json";
+
+export default function Api() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [refresh, setRefresh] = useState();
+  const [titles, setTitles] = useState([]);
+  const [categories, setCategories] = useState([]);
+  
+  useEffect(()=> {
+    let tmpTitles = [...titles];
+    let tmpCategories = [...categories];
+    fetch(URL)
+      .then(response => response.json())
+      .then ((json) => {
+        let jsonLength = Object.keys(json).length;
+        for (let i = 0; i < jsonLength; i++) {
+          if (!tmpTitles.includes(json[i].title)) {
+            tmpTitles.push(json[i].title)
+          }
+          if (!tmpCategories.includes(json[i].Categories[0].title)) {
+            tmpCategories.push(json[i].Categories[0].title)
+          }
+        }
+        setTitles(tmpTitles);
+        setCategories(tmpCategories);
+        setError(null);
+        setIsLoading(false);
+      },(error) => {
+        setError("Error retrieving activity!");
+        setIsLoading(false);
+        console.log(error);
+      })
+  },[refresh])
+
+  if (isLoading) {
+    return <View style={styles.container}><ActivityIndicator size="large"/></View>
+  } else if (error) {
+    return <View style={styles.container}><Text>{error}</Text></View>
+  } else {
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={styles.heading}>Kohteet</Text>
+          {titles.map((item, index) => (
+            <Text key={index}>{item}</Text>
+          ))}
+          <Text style={styles.heading}>Kategoriat</Text>
+          {categories.map((item, index) => (
+            <Text key={index}>{item}</Text>
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +64,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20
   },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginBottom: 20,
+  },  
+  activity: {
+    marginBottom: 10,
+  }
 });
